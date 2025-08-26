82c295a589fa0f45c094837b5494075c97cd0969
k-u-s-h-a-g-r-a-k-e-d-i-a <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2025-08-26 18:55:43 +0000

    initial commit

---
+++ Day/Code.c
@@ -0,0 +1,16 @@
+//Day 1 
+//Q1 (User Inputs, Operations & Output)
+//Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+int main()
+{
+int a , b , Sum;
+printf("Enter two numbers:");
+scanf("%d %d", &a , &b);
+Sum = a + b;
+printf("Sum:");
+printf("%d",Sum);
+ return 0;
+
+}
