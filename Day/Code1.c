ce214116a8a81d569075e86a461f9ba455d174eb
k-u-s-h-a-g-r-a-k-e-d-i-a <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2025-08-26 19:36:39 +0000

    final commit

---
+++ Day/Code1.c
@@ -0,0 +1,15 @@
+//Day 1 
+// Q2 (User Inputs, Operations & Output)
+// Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include <stdio.h>
+int main()
+{
+int a , b , Sum ;
+printf("Enter two numbers:");
+scanf("%d %d", &a , &b);
+Sum = a + b;
+printf("Sum=%d", Sum);
+ return 0;
+
+}
