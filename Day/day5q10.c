eba1d1fcba93f52e04cec59eb7c077a1329b9395
k-u-s-h-a-g-r-a-k-e-d-i-a <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2025-08-28 13:06:43 +0530

    Update day5q10.c

--- Day/day5q10.c
+++ Day/day5q10.c
@@ -1,9 +1,7 @@
 // Day 5
 // Q10 (User Inputs, Operations & Output)
 // Write a program to input time in seconds and convert it to hours:minutes:seconds format.
-
 #include <stdio.h>
-
 int main()
 {
     int sec, hours, minutes, seconds;
@@ -11,10 +9,9 @@ int main()
     printf("Enter time in seconds: ");
     scanf("%d", &sec);
     
-    // Calculate hours, minutes, and seconds
+    
     hours = sec / 3600;
-    sec = sec % 3600;
-    minutes = sec / 60;
+    minutes = (sec % 3600) / 60;
     seconds = sec % 60;
     
     printf("Time format: %02d:%02d:%02d\n", hours, minutes, seconds);
