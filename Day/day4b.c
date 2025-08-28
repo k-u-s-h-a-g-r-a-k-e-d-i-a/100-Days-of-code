f66c71eb4162f3085cea997047ebf1ad0d94531a
k-u-s-h-a-g-r-a-k-e-d-i-a <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2025-08-28 12:39:03 +0530

    Update day4b.c

--- Day/day4b.c
+++ Day/day4b.c
@@ -6,20 +6,10 @@
 
 int main()
 {
-    int n, sum = 0, i;
+    int n, sum = 0, ;
     
     printf("Enter the value of n: ");
     scanf("%d", &n);
-    
-    // Method 1: Using loop
-    for(i = 1; i <= n; i++)
-    {
-        sum = sum + i;
-    }
-    
-    printf("Sum of first %d natural numbers = %d\n", n, sum);
-    
-    // Method 2: Using formula (alternative approach)
     int formula_sum = n * (n + 1) / 2;
     printf("Using formula: Sum = %d\n", formula_sum);
     
