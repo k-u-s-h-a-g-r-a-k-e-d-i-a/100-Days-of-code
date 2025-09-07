a2cb32700eae94a3656d856680728a4d58cf8d28
k-u-s-h-a-g-r-a-k-e-d-i-a <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2025-09-07 12:53:47 +0530

    Create day29.c

---
+++ day29.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long factorial = 1;
+    
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    if(n < 0) {
+        printf("Factorial is not defined for negative numbers\n");
+    } else if(n == 0 || n == 1) {
+        printf("Factorial of %d is 1\n", n);
+    } else {
+        for(int i = 2; i <= n; i++) {
+            factorial *= i;
+        }
+        printf("Factorial of %d is %lld\n", n, factorial);
+    }
+    
+    return 0;
+}
