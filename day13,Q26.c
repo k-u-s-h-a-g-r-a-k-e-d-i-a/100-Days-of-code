e95da5f46d9095c30e5a4487b502562f366a410e
k-u-s-h-a-g-r-a-k-e-d-i-a <124067049+k-u-s-h-a-g-r-a-k-e-d-i-a@users.noreply.github.com>
2025-09-07 12:16:17 +0530

    Create day13,Q26.c

---
+++ day13,Q26.c
@@ -0,0 +1,14 @@
+// Q26 (Loops without Arrays/Strings)
+// Write a program to print numbers from 1 to n.
+#include <stdio.h>
+
+int main() {
+    int n,i;
+    printf("Enter n=");
+    scanf("%d",&n);
+    for (i = 1;i <= n;i++){
+        printf("%d\n",i);
+    }
+    
+    return 0;
+}
